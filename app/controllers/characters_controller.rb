ff54576b8bdda791f134c7e19fcea13593c5c213
jmelton22 <jmelton22@gmail.com>
2019-04-05 01:16:44 +0000

    Added new and show methods to Character and Stat controllers

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,16 @@
 class CharactersController < ApplicationController
+    
+    def new
+        @character = Character.new
+    end
+    
+    def show
+        @character = Character.find(params[:id])
+    end
+    
 end
+
+private
+    def character_params
+        params.require(:character).permit(:race, :characterClass, :backstory)
+    end
--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,2 +1,16 @@
 class StatsController < ApplicationController
+    
+    def new
+        @stat = Stat.new
+    end
+    
+    def show
+        @stat = Stat.find(params[:id])
+    end
+    
 end
+
+private
+    def stat_params
+        params.require(:stat).permit(:strength, :intelligence, :agility, :character_id)
+    end
