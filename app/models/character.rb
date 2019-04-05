b16eedb1ec868c801251d6ac85b29de6773ac31f
jmelton22 <jmelton22@gmail.com>
2019-04-05 02:12:10 +0000

    Created simple Character creation form and route for new action

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,6 +2,6 @@ class Character < ApplicationRecord
     has_one :stat, dependent: :destroy
     
     enum race: [:human, :dwarf, :dragon, :elf]
-    enum characterClass [:warrior, :mage, :lich, :assassin]
-    enum backstory [:hero, :fallen, :monk, :knight, :criminal]
+    enum characterClass: [:warrior, :mage, :lich, :assassin]
+    enum backstory: [:hero, :fallen, :monk, :knight, :criminal]
 end
