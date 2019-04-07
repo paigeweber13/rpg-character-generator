d9fa9a17e701a02380002720a588fe13bb09c1bb
jbSilo <jbricke4@uncc.edu>
2019-04-07 17:26:05 +0000

    General updates

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ApplicationRecord
     has_one :stat, dependent: :destroy
-    
+
     enum race: [:human, :dwarf, :dragon, :elf]
     enum characterClass: [:warrior, :mage, :lich, :assassin]
     enum backstory: [:hero, :fallen, :monk, :knight, :criminal]
