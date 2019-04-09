1e521a149e03bfab3747c2718f9a190f7ec76e07
Riley Weber <rileyw13@protonmail.com>
2019-04-09 17:33:56 -0400

    stats must now be positive.

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,3 +1,11 @@
 class Stat < ApplicationRecord
   belongs_to :character
+
+  # validation
+  validates_numericality_of :vitality, :greater_than => 0
+  validates_numericality_of :strength, :greater_than => 0
+  validates_numericality_of :dexterity, :greater_than => 0
+  validates_numericality_of :endurance, :greater_than => 0
+  validates_numericality_of :intelligence, :greater_than => 0
+  validates_numericality_of :luck, :greater_than => 0
 end
