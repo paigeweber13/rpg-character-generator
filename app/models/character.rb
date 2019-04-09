ea2302380f34072a742b29ee726b2234899e00e9
Riley Weber <rileyw13@protonmail.com>
2019-04-09 15:41:09 -0400

    implemented validation on models

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,4 +6,9 @@ class Character < ApplicationRecord
     enum characterClass: [:warrior, :mage, :lich, :assassin]
     enum backstory: [:hero, :fallen, :monk, :knight, :criminal]
     
+    # validation
+    validates :name, presence: true
+    validates :race, presence: true
+    validates :characterClass, presence: true
+    validates :backstory, presence: true
 end
