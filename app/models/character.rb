1cfd0052689171dfcf14c017db0fb0ed334fc380
jbSilo <jbricke4@uncc.edu>
2019-04-18 15:49:32 +0000

    Partial validations - include? results in error

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,4 +6,25 @@ class Character < ApplicationRecord
     enum characterClass: [:warrior, :mage, :lich, :assassin]
     enum backstory: [:hero, :fallen, :monk, :knight, :criminal]
     
+    validates :name, presence: true, length: {minimum: 2}
+    validates :race, :presence => true, if: :active_or_race?
+    validates :characterClass, presence: true, if: :active_or_characterClass?
+    validates :backstory, presence: true, if: :active_or_backstory?
+    
+    def active?
+        status == 'active'
+    end
+    
+    def active_or_race?
+        status.include?('race') || active?
+    end
+    
+    def active_or_characterClass?
+        status.to_s.include?('characterClass') || active?
+    end
+    
+    def active_or_backstory?
+        status.to_s.include?('backstory') || active?
+    end
+    
 end
