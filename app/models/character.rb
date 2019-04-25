2990476b5192ac93fc9cc9d3912f3ffbde40a88d
jmelton22 <jmelton22@gmail.com>
2019-04-25 13:27:22 +0000

    Created comment model and association with character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,6 +2,8 @@ class Character < ApplicationRecord
     has_one :stat, dependent: :destroy
     accepts_nested_attributes_for :stat, allow_destroy: true
     
+    has_many :comment, dependent: :destroy
+    
     enum race: [:human, :dwarf, :dragon, :elf]
     enum characterClass: [:warrior, :mage, :lich, :assassin]
     enum backstory: [:hero, :fallen, :monk, :knight, :criminal]
