44e48196d5115618c3a4d4deeb876481a273b1a1
jmelton22 <jmelton22@gmail.com>
2019-04-25 13:18:30 +0000

    Minor formatting edits

--- spec/integration/user_edits_a_character_spec.rb
+++ spec/integration/user_edits_a_character_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper.rb'
 
 feature 'User edits a character' do
-    scenario "User successfully navigates to the character list page from the homepage" do
+    scenario 'User successfully navigates to the character list page from the homepage' do
         visit root_path
         expect(page).to have_content('Welcome')
         click_link('Characters List')
         expect(page).to have_content('Listing Characters')
+    end
+    
+    scenario 'User successfully navigates to the edit form' do
+        visit characters_path
+        expect(page).to have_content('Listing Characters')
+        click_link('Edit')
+        expect(page).to have_content('Edit Character')
     end
 end
