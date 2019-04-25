c153419aab82c8b095b761ddc05f27d0f6294e3d
jmelton22 <jmelton22@gmail.com>
2019-04-25 13:34:49 +0000

    Add create and destroy methods to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,15 @@
 class CommentsController < ApplicationController
   def create
+    @character = Character.find(params[:character_id])
+    @comment = @character.comments.create(comment_params)
+    redirect_to character_path(@character)
   end
 
   def destroy
+    @character = Character.find(params[:character_id])
+    @comment = @character.comments.find(params[:id])
+    @comment.destroy
+    redirect_to character_path(@character)
   end
   
   private
